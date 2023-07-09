53dfac6b7ce785f1919a534c9c087dcb73ad7d87
Gaurav kumar <102864247+yashswag22@users.noreply.github.com>
2023-07-09 13:45:12 +0530

    Create Print n times.cpp
    Print any string n time using recursion.
    #string #recursion #codingninjas

---
+++ Print n times.cpp
@@ -0,0 +1,16 @@
+void createarray (vector<int> &v,int n)
+{
+   if(n<1)
+   return;
+    
+    createarray(v,n-1);
+    v.push_back(n);
+   return ;
+}
+
+
+vector<int> printNos(int x) {
+  vector<int> v;
+  createarray(v,x);
+  return v;
+}
