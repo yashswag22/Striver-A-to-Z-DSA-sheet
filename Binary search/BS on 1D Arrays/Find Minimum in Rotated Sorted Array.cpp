92255c5ca63d9ddd1e009c38178cf5b6291f8673
Gaurav kumar <102864247+yashswag22@users.noreply.github.com>
2023-08-28 23:40:44 +0530

    Update Find Minimum in Rotated Sorted Array.cpp
    updated with striver method

--- Binary search/BS on 1D Arrays/Find Minimum in Rotated Sorted Array.cpp
+++ Binary search/BS on 1D Arrays/Find Minimum in Rotated Sorted Array.cpp
@@ -3,16 +3,38 @@ int findMin(vector<int>& arr)
 	int n= arr.size();
 	int low =0,high=n-1;
 	int mini = INT_MAX;
-	while(low <=high)
+	// ******** my method *******
+	// while(low <=high)
+	// {
+	// 	int mid = (low+high)/2;
+	// 	mini = min(arr[mid],mini);
+
+	// 	if(arr[mid]< arr[high])
+	// 	high = mid-1;
+	// 	else
+	// 	low = mid + 1;
+	// }
+	// ******* striver method *******
+	while(low <= high)
 	{
-		int mid = (low+high)/2;
-		mini = min(arr[mid],mini);
+		int mid = (low + high)/2;
+		
+		if(arr[low]<= arr[high])
+		{
+			mini = min(arr[low],mini);
+			break;
+		}
 
-		if(arr[mid]< arr[high])
-		high = mid-1;
+		if(arr[low]<= arr[mid])
+		{
+			mini = min(arr[low],mini);
+			low = mid +1;
+		}
 		else
-		low = mid + 1;
+		{
+			mini = min(arr[mid],mini);
+			high = mid -1;
+		}
 	}
-
 	return mini;
 }
