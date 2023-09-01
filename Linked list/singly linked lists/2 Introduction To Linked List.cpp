58dbb0e84aa945847ede1237f1f78aba2e7931ad
Gaurav kumar <102864247+yashswag22@users.noreply.github.com>
2023-09-02 02:23:55 +0530

    Create 2 Introduction To Linked List.cpp

---
+++ Linked list/singly linked lists/2 Introduction To Linked List.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition of linked list
+ * class Node {
+ *
+ * public:
+ *     int data;
+ *     Node* next;
+ *     Node() : data(0), next(nullptr) {}
+ *     Node(int x) : data(x), next(nullptr) {}
+ *     Node(int x, Node* next) : data(x), next(next) {}
+ * };
+ */
+
+Node* insertAtFirst(Node* list, int newValue) {
+    Node* first = new Node;
+    first->data = newValue;
+    first->next = list;
+    list=first;
+    return list;
+}
