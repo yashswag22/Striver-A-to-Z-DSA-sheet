1bbb59bdb19c2d685adafd8497fe07468619c115
Gaurav kumar <102864247+yashswag22@users.noreply.github.com>
2023-10-04 03:44:57 +0530

    Create Add one to a number represented as Linked List.cpp

---
+++ Add one to a number represented as Linked List.cpp
@@ -0,0 +1,42 @@
+Node* recursive_reverse2(Node * cur, Node * t)
+ {
+     if(cur == nullptr)
+     return t;
+     Node * nxtnode = cur->next;
+     cur->next =t;
+     return (recursive_reverse2(nxtnode, cur));
+
+ }
+
+
+ Node *addOne(Node *head)
+ {
+    Node * temp = head;
+    temp = recursive_reverse2(temp,nullptr);
+
+    Node * temp2 = temp;
+    while(temp2!= nullptr)
+    {
+     if (temp2->data != 9) {
+       temp2->data++;
+       break;
+     }
+     else if(temp2->data==9 && temp2->next == nullptr)
+     {
+         temp2->data=0;
+         Node * dummy = new Node();
+         dummy->data =1;
+         temp2->next = dummy;
+         dummy->next = nullptr;
+         break;
+     }
+     else if(temp2->data ==9)
+     {
+         temp2->data =0;
+         temp2 = temp2->next;
+     }
+
+    }
+    temp = recursive_reverse2(temp, nullptr);
+    return temp;
+ }
