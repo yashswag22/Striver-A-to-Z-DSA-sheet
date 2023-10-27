8f342c43ac071a7b71d7bd8267005e3a13bea5bc
Gaurav kumar <102864247+yashswag22@users.noreply.github.com>
2023-10-27 16:02:54 +0530

    Create 4 Longest Common Prefix.cpp

---
+++ String/ Basic and Easy String Problems/4 Longest Common Prefix.cpp
@@ -0,0 +1,25 @@
+string commonPrefix(vector<string>& arr,int n){
+	string s = arr[0];
+	for(int i=1;i<n;i++)
+	{
+		string temp = "";
+		string cur = arr[i];
+
+		int a = s.size();
+		int b = cur.size();
+
+		int siz = min(a,b);
+		for(int j = 0;j<siz;j++)
+		{
+			if(s[j]==cur[j])
+			temp+=s[j];
+			else
+			break;
+		}	
+
+		s = temp;
+
+	}
+	if(s == "") return "-1";
+	return s;
+}
