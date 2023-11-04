ca4abbc5849a5e3da92729985c06300040b985bb
Gaurav kumar <102864247+yashswag22@users.noreply.github.com>
2023-11-04 09:09:37 +0530

    Create 7 Check if two Strings are anagrams of each other.cpp

---
+++ String/ Basic and Easy String Problems/7 Check if two Strings are anagrams of each other.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+bool isAnagram(string str1, string str2)
+{      if( str1.size() != str2.size())
+        return false;
+        // ------------ method 1 ------------
+    // sort(str1.begin(),str1.end());
+    // sort(str2.begin(),str2.end());
+
+    // if(str1 == str2) 
+    // return true;
+    // else 
+    // return false;
+
+       // ---------- method 2 ----------------
+    else
+    {
+        map<char,int>mpp;
+
+        for(int i =0;i<str1.size();i++)
+        {
+            mpp[str1[i]]++;
+        }
+
+        for(int i =0;i<str2.size();i++)
+        {
+            mpp[str2[i]]--;
+        }
+
+        for(int i =0;i<str2.size();i++)
+        {
+            if(mpp[str2[i]] != 0)
+            return false;
+        }
+        return true;
+    }
+}
