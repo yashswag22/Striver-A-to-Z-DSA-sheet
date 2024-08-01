c1f7cca08c78da4fa65c1bed2265aaaf62c4fa94
Gaurav kumar <102864247+yashswag22@users.noreply.github.com>
2024-08-02 02:00:53 +0530

    Create 14. Recover BST | Correct BST with two nodes swapped.cpp

---
+++ Step 14 : Binary Search Trees [Concept and Problems]/Lec 2: Practice Problems/14. Recover BST | Correct BST with two nodes swapped.cpp
@@ -0,0 +1,70 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    stack<TreeNode*> s;
+
+    void pushleft(TreeNode* root){
+        while(root != NULL){
+            s.push(root);
+            root = root->left;
+        }
+    }
+    TreeNode* next(){
+        if(!s.empty()){
+            TreeNode* temp = s.top();
+            s.pop();
+            if(temp->right) pushleft(temp->right);
+            return temp;
+        }
+
+        return NULL;
+    }
+    void recoverTree(TreeNode* root) {
+
+        TreeNode * first  = NULL;
+        TreeNode * second = NULL;
+
+         TreeNode * prev = NULL;
+         TreeNode * curr = NULL;
+
+        int irregpair = 0;
+
+        pushleft(root);
+
+        prev = next();
+         curr = next();
+
+        while(curr!=NULL && irregpair <2){
+            // curr = next();
+
+            if(curr -> val < prev -> val ){
+                if(irregpair == 0){
+                    first = prev;
+                    second = curr;
+                    irregpair++;
+                }
+                else if(irregpair == 1){
+                    second = curr;
+                    irregpair++;
+                }
+            }
+
+            prev = curr;
+             curr = next();
+        }
+
+        swap(first->val,second->val);
+        
+    }
+};
